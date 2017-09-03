016a3bea68e23bf008b105dbf74839db01145bff
lisdude <git@lisdude.com>
2017-09-02 21:32:46 -0500

    Add Toastsoft extensions: frandom, round, distance, relative_heading, memory_info, ftime, panic, locate_by_name, strtol, parse_ansi, remove_ansi

--- extensions.cc
+++ extensions.cc
@@ -1,186 +1,420 @@
-/******************************************************************************
-  Copyright (c) 1995, 1996 Xerox Corporation.  All rights reserved.
-  Portions of this code were written by Stephen White, aka ghond.
-  Use and copying of this software and preparation of derivative works based
-  upon this software are permitted.  Any distribution of this software or
-  derivative works must comply with all applicable United States export
-  control laws.  This software is made available AS IS, and Xerox Corporation
-  makes no warranty about the software, its performance or its conformity to
-  any specification.  Any person obtaining a copy of this software is requested
-  to send their name and post office or electronic mail address to:
-    Pavel Curtis
-    Xerox PARC
-    3333 Coyote Hill Rd.
-    Palo Alto, CA 94304
-    Pavel@Xerox.Com
- *****************************************************************************/
-
-/* Extensions to the MOO server
-
- * This module contains some examples of how to extend the MOO server using
- * some of the interfaces exported by various other modules.  The examples are
- * all commented out, since they're really not all that useful in general; they
- * were written primarily to test out the interfaces they use.
- *
- * The uncommented parts of this module provide a skeleton for any module that
- * implements new MOO built-in functions.  Feel free to replace the
- * commented-out bits with your own extensions; in future releases, you can
- * just replace the distributed version of this file (which will never contain
- * any actually useful code) with your own edited version as a simple way to
- * link in your extensions.
- */
-
-#define EXAMPLE 0
+#include <sys/time.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "bf_register.h"
 #include "functions.h"
 #include "db_tune.h"
-
-#if EXAMPLE
-
 #include "my-unistd.h"
-
 #include "log.h"
 #include "net_multi.h"
 #include "storage.h"
 #include "tasks.h"
-
-typedef struct stdin_waiter {
-    struct stdin_waiter *next;
-    vm the_vm;
-} stdin_waiter;
-
-static stdin_waiter *waiters = 0;
-
-static task_enum_action
-stdin_enumerator(task_closure closure, void *data)
-{
-    stdin_waiter **ww;
-
-    for (ww = &waiters; *ww; ww = &((*ww)->next)) {
-	stdin_waiter *w = *ww;
-	const char *status = (w->the_vm->task_id & 1
-			      ? "stdin-waiting"
-			      : "stdin-weighting");
-	task_enum_action tea = (*closure) (w->the_vm, status, data);
-
-	if (tea == TEA_KILL) {
-	    *ww = w->next;
-	    myfree(w, M_TASK);
-	    if (!waiters)
-		network_unregister_fd(0);
-	}
-	if (tea != TEA_CONTINUE)
-	    return tea;
-    }
-
-    return TEA_CONTINUE;
-}
-
-static void
-stdin_readable(int fd, void *data)
-{
-    char buffer[1000];
-    int n;
-    Var v;
-    stdin_waiter *w;
-
-    if (data != &waiters)
-	panic("STDIN_READABLE: Bad data!");
-
-    if (!waiters) {
-	errlog("STDIN_READABLE: Nobody cares!\n");
-	return;
-    }
-    n = read(0, buffer, sizeof(buffer));
-    buffer[n] = '\0';
-    while (n)
-	if (buffer[--n] == '\n')
-	    buffer[n] = 'X';
-
-    if (buffer[0] == 'a') {
-	v.type = TYPE_ERR;
-	v.v.err = E_NACC;
-    } else {
-	v.type = TYPE_STR;
-	v.v.str = str_dup(buffer);
-    }
-
-    resume_task(waiters->the_vm, v);
-    w = waiters->next;
-    myfree(waiters, M_TASK);
-    waiters = w;
-    if (!waiters)
-	network_unregister_fd(0);
-}
-
-static enum error
-stdin_suspender(vm the_vm, void *data)
-{
-    stdin_waiter *w = data;
-
-    if (!waiters)
-	network_register_fd(0, stdin_readable, 0, &waiters);
-
-    w->the_vm = the_vm;
-    w->next = waiters;
-    waiters = w;
-
-    return E_NONE;
-}
-
-static package
-bf_read_stdin(Var arglist, Byte next, void *vdata, Objid progr)
-{
-    stdin_waiter *w = mymalloc(sizeof(stdin_waiter), M_TASK);
-
-    return make_suspend_pack(stdin_suspender, w);
-}
-#endif				/* EXAMPLE */
-
-#define STUPID_VERB_CACHE 1
-#ifdef STUPID_VERB_CACHE
 #include "utils.h"
+#include "numbers.h"
+#include "list.h"
+#include "my-stdlib.h"
+#include "random.h"
+#include "name_lookup.h"
+#include "db.h"
+#include "server.h"
 
-static package
-bf_verb_cache_stats(Var arglist, Byte next, void *vdata, Objid progr)
+// Millisecond time for OS X
+#ifdef __MACH__
+#include <mach/clock.h>
+#endif
+
+
+// Forward declarations
+extern Var string_to_moo_type(char *, bool, bool);  // from extension-sqlite.c
+
+/* Returns a float of the time (including milliseconds)
+   Optional arguments specify monotonic time; 1: Monotonic. 2. Monotonic raw.
+   (seconds since an arbitrary period of time. More useful for timing
+   since its not affected by NTP or other time changes.) */
+    static package
+bf_ftime(Var arglist, Byte next, void *vdata, Objid progr)
 {
-    Var r;
+#ifdef __MACH__
+    // OS X only provides SYSTEM_CLOCK for monotonic time, so our arguments don't matter.
+    clock_id_t clock_type = (arglist.v.list[0].v.num == 0 ? CALENDAR_CLOCK : SYSTEM_CLOCK);
+#else
+    // Other OSes provide MONOTONIC_RAW and MONOTONIC, so we'll check args for 2(raw) or 1.
+    clockid_t clock_type = 0;
+    if (arglist.v.list[0].v.num == 0)
+        clock_type = CLOCK_REALTIME;
+    else
+#if defined(__WIN32) || defined(__CYGWIN__)
+        // CYGWIN doesn't give us MONOTONIC_RAW
+        clock_type = CLOCK_MONOTONIC;
+#else
+    clock_type = arglist.v.list[1].v.num == 2 ? CLOCK_MONOTONIC_RAW : CLOCK_MONOTONIC;
+#endif
+#endif
+
+    struct timespec ts;
+
+#ifdef __MACH__
+    // OS X lacks clock_gettime, use clock_get_time instead
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), clock_type, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts.tv_sec = mts.tv_sec;
+    ts.tv_nsec = mts.tv_nsec;
+#else
+    clock_gettime(clock_type, &ts);
+#endif
+
+    Var r = new_float((double)ts.tv_sec + (double)ts.tv_nsec / 1000000000.0);
 
     free_var(arglist);
-
-    if (!is_wizard(progr)) {
-	return make_error_pack(E_PERM);
-    }
-    r = db_verb_cache_stats();
-
     return make_var_pack(r);
 }
 
-static package
-bf_log_cache_stats(Var arglist, Byte next, void *vdata, Objid progr)
+    static package
+bf_strtol(Var arglist, Byte next, void *vdata, Objid progr)
 {
+    Var r;
+    int base = 0;
+
+    if (arglist.v.list[0].v.num == 2)
+        base = arglist.v.list[2].v.num;
+
+    r.v.num = strtol(arglist.v.list[1].v.str, NULL, base);
+
+    free_var(arglist);
+    return make_var_pack(r);
+}
+
+
+    static package
+bf_locate_by_name(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    // Locates objects in the database by name. Wizard only because of the potential to lock up the whole MOO.
+
+    if(!is_wizard(progr))
+    {
+        free_var(arglist);
+        return make_error_pack(E_PERM);
+    }
+
+    int x = 1, case_matters = 0;
+    Var ret = new_list(0), tmp, name;
+
+    tmp.type = TYPE_OBJ;
+
+    if (arglist.v.list[0].v.num == 2)
+        case_matters = is_true(arglist.v.list[2]);
+
+
+    for (x = 1; x < db_last_used_objid(); x++)
+    {
+        if (valid(x))
+        {
+            db_find_property(Var::new_obj(x), "name", &name);
+            if (strindex(name.v.str, memo_strlen(name.v.str), arglist.v.list[1].v.str, memo_strlen(arglist.v.list[1].v.str), case_matters))
+            {
+                tmp.v.obj = x;
+                ret = listappend(ret, tmp);
+            }
+        }
+    }
+
+    free_var(tmp);
     free_var(arglist);
 
-    if (!is_wizard(progr)) {
-	return make_error_pack(E_PERM);
-    }
-    db_log_cache_stats();
-
-    return no_var_pack();
+    return make_var_pack(ret);
 }
-#endif
+
+    static package
+bf_distance(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    double ret = 0.0, tmp = 0.0;
+    int count;
+
+    for (count = 1; count <= arglist.v.list[1].v.list[0].v.num; count++)
+    {
+        if ((arglist.v.list[1].v.list[count].type != TYPE_INT && arglist.v.list[1].v.list[count].type != TYPE_FLOAT) || (arglist.v.list[2].v.list[count].type != TYPE_INT && arglist.v.list[2].v.list[count].type != TYPE_FLOAT))
+        {
+            free_var(arglist);
+            return make_error_pack(E_TYPE);
+        }
+        else
+        {
+            tmp = (arglist.v.list[2].v.list[count].type == TYPE_INT ? (double)arglist.v.list[2].v.list[count].v.num : *arglist.v.list[2].v.list[count].v.fnum) - (arglist.v.list[1].v.list[count].type == TYPE_INT ? (double)arglist.v.list[1].v.list[count].v.num : *arglist.v.list[1].v.list[count].v.fnum);
+            ret = ret + (tmp * tmp);
+        }
+    }
+
+    free_var(arglist);
+
+    Var s;
+    s = new_float(sqrt(ret));
+
+    return make_var_pack(s);
+}
+
+    static package
+bf_relative_heading(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    if (arglist.v.list[1].v.list[1].type != TYPE_FLOAT || arglist.v.list[1].v.list[2].type != TYPE_FLOAT || arglist.v.list[1].v.list[3].type != TYPE_FLOAT || arglist.v.list[2].v.list[1].type != TYPE_FLOAT || arglist.v.list[2].v.list[2].type != TYPE_FLOAT || arglist.v.list[2].v.list[3].type != TYPE_FLOAT) {
+        free_var(arglist);
+        return make_error_pack(E_TYPE);
+    }
+
+    double dx = *arglist.v.list[2].v.list[1].v.fnum - *arglist.v.list[1].v.list[1].v.fnum;
+    double dy = *arglist.v.list[2].v.list[2].v.fnum - *arglist.v.list[1].v.list[2].v.fnum;
+    double dz = *arglist.v.list[2].v.list[3].v.fnum - *arglist.v.list[1].v.list[3].v.fnum;
+
+    double xy = 0.0;
+    double z = 0.0;
+
+    xy = atan2(dy, dx) * 57.2957795130823;
+
+    if (xy < 0.0)
+        xy = xy + 360.0;
+
+    z = atan2(dz, sqrt((dx * dx) + (dy * dy))) * 57.2957795130823;
+
+    Var s = new_list(2);
+    s.v.list[1].type = TYPE_INT;
+    s.v.list[1].v.num = (int)xy;
+    s.v.list[2].type = TYPE_INT;
+    s.v.list[2].v.num = (int)z;
+
+    free_var(arglist);
+
+    return make_var_pack(s);
+}
+
+    static package
+bf_memory_info(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    // Values are returned in pages. To get KB, multiply by 4.
+    free_var(arglist);
+    Var s = new_list(5);
+
+    long double size, resident, share, text, lib, data, dt;
+
+    FILE *f = fopen("/proc/self/statm", "r");
+
+    if (!f)
+        return make_error_pack(E_NACC);
+
+    if (fscanf(f, "%Lf %Lf %Lf %Lf %Lf %Lf %Lf",
+                &size, &resident, &share, &text, &lib, &data, &dt) != 7)
+    {
+        fclose(f);
+        return make_error_pack(E_NACC);
+    }
+
+    fclose(f);
+
+    s.v.list[1] = new_float(size);           // Total program size
+    s.v.list[2] = new_float(resident);       // Resident set size
+    s.v.list[3] = new_float(share);          // Shared pages from shared mappings
+    s.v.list[4] = new_float(text);           // Text (code)
+    s.v.list[5] = new_float(data);           // Data + stack
+
+    return make_var_pack(s);
+}
+
+    static package
+bf_panic(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    const char *msg;
+
+    if(!is_wizard(progr)) {
+        free_var(arglist);
+        return make_error_pack(E_PERM);
+    }
+
+    if(arglist.v.list[0].v.num) {
+        msg=str_dup(arglist.v.list[1].v.str);
+    } else {
+        msg="";
+    }
+
+    free_var(arglist);
+    panic(msg);
+
+    return make_error_pack(E_NONE);
+}
+
+    static package
+bf_frandom(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    double fmin = (arglist.v.list[0].v.num > 1 ? *arglist.v.list[1].v.fnum : 0.0);
+    double fmax = (arglist.v.list[0].v.num > 1 ? *arglist.v.list[2].v.fnum : *arglist.v.list[1].v.fnum);
+
+    free_var(arglist);
+
+    double f = (double)rand() / RAND_MAX;
+    f = fmin + f * (fmax - fmin);
+
+    return make_var_pack(new_float(f));
+
+}
+
+    static package
+bf_round(Var arglist, Byte next, void *vdata, Objid progr)
+{
+    double r = round((double)*arglist.v.list[1].v.fnum);
+
+    free_var(arglist);
+
+    return make_var_pack(new_float(r));
+}
+
+// ============= ANSI ===============
+    static package
+bf_parse_ansi(Var arglist, Byte next, void *vdata, Objid progr)
+{
+#define ANSI_TAG_TO_CODE(tag, code, case_matters)			\
+    {									\
+        stream_add_strsub(str, reset_stream(tmp), tag, code, case_matters); \
+        stream_add_string(tmp, reset_stream(str));				\
+    }
+
+    Var r;
+    r.type = TYPE_STR;
+
+    Stream *str = new_stream(50);
+    Stream *tmp = new_stream(50);
+    const char *random_codes[] = {"\e[31m", "\e[32m", "\e[33m", "\e[34m", "\e[35m", "\e[35m", "\e[36m"};
+
+    stream_add_string(tmp, arglist.v.list[1].v.str);
+    free_var(arglist);
+
+    ANSI_TAG_TO_CODE("[red]",        "\e[31m",   0);
+    ANSI_TAG_TO_CODE("[green]",      "\e[32m",   0);
+    ANSI_TAG_TO_CODE("[yellow]",     "\e[33m",   0);
+    ANSI_TAG_TO_CODE("[blue]",       "\e[34m",   0);
+    ANSI_TAG_TO_CODE("[purple]",     "\e[35m",   0);
+    ANSI_TAG_TO_CODE("[cyan]",       "\e[36m",   0);
+    ANSI_TAG_TO_CODE("[normal]",     "\e[0m",    0);
+    ANSI_TAG_TO_CODE("[inverse]",    "\e[7m",    0);
+    ANSI_TAG_TO_CODE("[underline]",  "\e[4m",    0);
+    ANSI_TAG_TO_CODE("[bold]",       "\e[1m",    0);
+    ANSI_TAG_TO_CODE("[bright]",     "\e[1m",    0);
+    ANSI_TAG_TO_CODE("[unbold]",     "\e[22m",   0);
+    ANSI_TAG_TO_CODE("[blink]",      "\e[5m",    0);
+    ANSI_TAG_TO_CODE("[unblink]",    "\e[25m",   0);
+    ANSI_TAG_TO_CODE("[magenta]",    "\e[35m",   0);
+    ANSI_TAG_TO_CODE("[unbright]",   "\e[22m",   0);
+    ANSI_TAG_TO_CODE("[white]",      "\e[37m",   0);
+    ANSI_TAG_TO_CODE("[gray]",       "\e[1;30m", 0);
+    ANSI_TAG_TO_CODE("[grey]",       "\e[1;30m", 0);
+    ANSI_TAG_TO_CODE("[beep]",       "\a",       0);
+    ANSI_TAG_TO_CODE("[black]",      "\e[30m",   0);
+    ANSI_TAG_TO_CODE("[b:black]",   "\e[40m",   0);
+    ANSI_TAG_TO_CODE("[b:red]",     "\e[41m",   0);
+    ANSI_TAG_TO_CODE("[b:green]",   "\e[42m",   0);
+    ANSI_TAG_TO_CODE("[b:yellow]",  "\e[43m",   0);
+    ANSI_TAG_TO_CODE("[b:blue]",    "\e[44m",   0);
+    ANSI_TAG_TO_CODE("[b:magenta]", "\e[45m",   0);
+    ANSI_TAG_TO_CODE("[b:purple]",  "\e[45m",   0);
+    ANSI_TAG_TO_CODE("[b:cyan]",    "\e[46m",   0);
+    ANSI_TAG_TO_CODE("[b:white]",   "\e[47m",   0);
+
+    char *t = reset_stream(tmp);
+    while (*t) {
+        if (!mystrncasecmp(t, "[random]", 8)) {
+            stream_add_string(str, random_codes[RANDOM() % 6]);
+            t += 8;
+        } else
+            stream_add_char(str, *t++);
+    }
+
+    stream_add_strsub(tmp, reset_stream(str), "[null]", "", 0);
+
+    ANSI_TAG_TO_CODE("[null]", "", 0);
+
+    r.v.str = str_dup(reset_stream(tmp));
+
+    free_stream(tmp);
+    free_stream(str);
+    return make_var_pack(r);
+
+#undef ANSI_TAG_TO_CODE
+}
 
 
-void
+    static package
+bf_remove_ansi(Var arglist, Byte next, void *vdata, Objid progr)
+{
+
+#define MARK_FOR_REMOVAL(tag)					\
+    {								\
+        stream_add_strsub(tmp, reset_stream(tmp), tag, "", 0);	\
+    }
+    Var r;
+    Stream *tmp;
+
+    tmp = new_stream(50);
+    stream_add_string(tmp, arglist.v.list[1].v.str);
+    free_var(arglist);
+
+    MARK_FOR_REMOVAL("[red]");
+    MARK_FOR_REMOVAL("[green]");
+    MARK_FOR_REMOVAL("[yellow]");
+    MARK_FOR_REMOVAL("[blue]");
+    MARK_FOR_REMOVAL("[purple]");
+    MARK_FOR_REMOVAL("[cyan]");
+    MARK_FOR_REMOVAL("[normal]");
+    MARK_FOR_REMOVAL("[inverse]");
+    MARK_FOR_REMOVAL("[underline]");
+    MARK_FOR_REMOVAL("[bold]");
+    MARK_FOR_REMOVAL("[bright]");
+    MARK_FOR_REMOVAL("[unbold]");
+    MARK_FOR_REMOVAL("[blink]");
+    MARK_FOR_REMOVAL("[unblink]");
+    MARK_FOR_REMOVAL("[magenta]");
+    MARK_FOR_REMOVAL("[unbright]");
+    MARK_FOR_REMOVAL("[white]");
+    MARK_FOR_REMOVAL("[gray]");
+    MARK_FOR_REMOVAL("[grey]");
+    MARK_FOR_REMOVAL("[beep]");
+    MARK_FOR_REMOVAL("[black]");
+    MARK_FOR_REMOVAL("[b:black]");
+    MARK_FOR_REMOVAL("[b:red]");
+    MARK_FOR_REMOVAL("[b:green]");
+    MARK_FOR_REMOVAL("[b:yellow]");
+    MARK_FOR_REMOVAL("[b:blue]");
+    MARK_FOR_REMOVAL("[b:magenta]");
+    MARK_FOR_REMOVAL("[b:purple]");
+    MARK_FOR_REMOVAL("[b:cyan]");
+    MARK_FOR_REMOVAL("[b:white]");
+    MARK_FOR_REMOVAL("[random]");
+    MARK_FOR_REMOVAL("[null]");
+
+    r.type = TYPE_STR;
+    r.v.str = str_dup(reset_stream(tmp));
+
+    free_stream(tmp);
+    return make_var_pack(r);
+
+#undef MARK_FOR_REMOVAL
+}
+//==============================================================
+
+    void
 register_extensions()
 {
-#if EXAMPLE
-    register_task_queue(stdin_enumerator);
-    register_function("read_stdin", 0, 0, bf_read_stdin);
-#endif
-#ifdef STUPID_VERB_CACHE
-    register_function("log_cache_stats", 0, 0, bf_log_cache_stats);
-    register_function("verb_cache_stats", 0, 0, bf_verb_cache_stats);
-#endif
+    register_function("frandom", 1, 2, bf_frandom, TYPE_FLOAT, TYPE_FLOAT);
+    register_function("round", 1, 1, bf_round, TYPE_FLOAT);
+    register_function("distance", 2, 2, bf_distance, TYPE_LIST, TYPE_LIST);
+    register_function("relative_heading", 2, 2, bf_relative_heading, TYPE_LIST, TYPE_LIST);
+    register_function("memory_info", 0, 0, bf_memory_info);
+    register_function("ftime", 0, 1, bf_ftime, TYPE_INT);
+    register_function("panic", 0, 1, bf_panic, TYPE_STR);
+    register_function("locate_by_name", 1, 2, bf_locate_by_name, TYPE_STR);
+    register_function("strtol", 1, 2, bf_strtol, TYPE_STR, TYPE_INT);
+    // ======== ANSI ===========
+    register_function("parse_ansi", 1, 1, bf_parse_ansi, TYPE_STR);
+    register_function("remove_ansi", 1, 1, bf_remove_ansi, TYPE_STR);
 }
--- version.cc
+++ version.cc
@@ -53,8 +53,8 @@
 #include "version_src.h"
 #ifndef VERSION_MAJOR
 #  define VERSION_MAJOR 2
-#  define VERSION_MINOR 1
-#  define VERSION_RELEASE 1
+#  define VERSION_MINOR 0
+#  define VERSION_RELEASE 3
 #endif
 #ifndef VERSION_EXT
 #  define VERSION_EXT "_toastsoft"
