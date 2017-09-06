e05f2ef7c7298916373311bee36581fc23898efd
lisdude <git@lisdude.com>
2017-09-06 14:20:20 -0500

    Bump Toastsoft version to 1 now that all of the essentials are in.

--- version.cc
+++ version.cc
@@ -53,8 +53,8 @@
 #include "version_src.h"
 #ifndef VERSION_MAJOR
 #  define VERSION_MAJOR 2
-#  define VERSION_MINOR 0
-#  define VERSION_RELEASE 4
+#  define VERSION_MINOR 1
+#  define VERSION_RELEASE 0
 #endif
 #ifndef VERSION_EXT
 #  define VERSION_EXT "_toastsoft"
