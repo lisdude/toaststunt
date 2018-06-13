5d28b2143d23a0efdef39b6f6a5e42373a5c4113
lisdude <git@lisdude.com>
2018-06-13 06:16:36 -0500

    Bump version.

--- version.cc
+++ version.cc
@@ -54,7 +54,7 @@
 #ifndef VERSION_MAJOR
 #  define VERSION_MAJOR 2
 #  define VERSION_MINOR 3
-#  define VERSION_RELEASE 8
+#  define VERSION_RELEASE 9
 #endif
 #ifndef VERSION_EXT
 #  define VERSION_EXT ""
