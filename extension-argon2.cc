d52a57378c13c2286273ad0be1950dff64a6d9d8
lisdude <git@lisdude.com>
2019-01-13 12:36:23 -0600

    Remove pointless memory wipe.

--- extension-argon2.cc
+++ extension-argon2.cc
@@ -8,9 +8,6 @@
 #include "utils.h"
 #include "log.h"
 
-#define __STDC_WANT_LIB_EXT1__ 1
-void *erase_from_memory(void *pointer, size_t size_data, size_t size_to_remove);
-
 static package
 bf_argon2(Var arglist, Byte next, void *vdata, Objid progr)
 {
@@ -63,8 +60,6 @@ bf_argon2(Var arglist, Byte next, void *vdata, Objid progr)
     r.type = TYPE_STR;
     r.v.str = encoded;
     free_var(arglist);
-    // This is probably not necessary since we're throwing the encoded string around anyway, but whatever.
-    erase_from_memory(out, outlen, outlen);
     free(out);
 
     return make_var_pack(r);
@@ -95,19 +90,6 @@ bf_argon2_verify(Var arglist, Byte next, void *vdata, Objid progr)
         r.v.num = 1;
 
     return make_var_pack(r);
-}
-
-void *erase_from_memory(void *pointer, size_t size_data, size_t size_to_remove) {
-  #ifdef __STDC_LIB_EXT1__
-   memset_s(pointer, size_data, 0, size_to_remove);
-  #else
-   if(size_to_remove > size_data) size_to_remove = size_data;
-	 volatile unsigned char *p = (volatile unsigned char *)pointer;
-	 while (size_to_remove--){
-		 *p++ = 0;
-	 }
-  #endif
-	return pointer;
 }
 
 void
