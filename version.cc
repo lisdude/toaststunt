602d7059272b434888444bc78c7f1d8578a05ebb
distantorigin <admin@anomalousabode.com>
2019-02-01 05:56:55 -0500

    Bump version to 2.5.

--- version.cc
+++ version.cc
@@ -53,8 +53,8 @@
 #include "version_src.h"
 #ifndef VERSION_MAJOR
 #  define VERSION_MAJOR 2
-#  define VERSION_MINOR 4
-#  define VERSION_RELEASE 6
+#  define VERSION_MINOR 5
+#  define VERSION_RELEASE 0
 #endif
 #ifndef VERSION_EXT
 #  define VERSION_EXT ""
