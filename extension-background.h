c0675a948f6f74b86374727115a27bcd8c6ed543
lisdude <git@lisdude.com>
2019-02-07 20:33:53 -0600

    Only spawn two worker threads.
    The majority of MOOs could likely get away with only using one.
    Either way, this will reduce memory usage by default and people
    who need more can enable more.

--- extension-background.h
+++ extension-background.h
@@ -15,7 +15,7 @@
 #include "thpool.h"         // thread pool
 
 #define THREAD_MOO_VERSION      "2.4"   // Version of our MOO threading library.
-#define TOTAL_BACKGROUND_THREADS 4      // The total number of background threads running in the pool.
+#define TOTAL_BACKGROUND_THREADS 2      // The total number of background threads running in the pool.
 #define MAX_BACKGROUND_THREADS  20      /* The total number threads allowed to be run from within the MOO.
                                            Can be overridden with $server_options.max_background_threads */
 
