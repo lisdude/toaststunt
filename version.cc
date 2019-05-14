1e3386d0e26a7cbf792b3c0ebe3a356e8941ae48
lisdude <git@lisdude.com>
2019-05-14 00:13:46 -0500

    Increment version.

--- version.cc
+++ version.cc
@@ -55,7 +55,7 @@
 #ifndef VERSION_MAJOR
 #  define VERSION_MAJOR 2
 #  define VERSION_MINOR 5
-#  define VERSION_RELEASE 4
+#  define VERSION_RELEASE 5
 #endif
 #ifndef VERSION_EXT
 #  define VERSION_EXT ""
