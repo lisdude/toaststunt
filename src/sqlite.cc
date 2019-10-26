e3c1a3c6399bdb8f8e1b69e94d4a4e675002a68d
lisdude <git@lisdude.com>
2019-10-26 00:27:42 -0500

    Shorten the names of limit categories.

--- src/sqlite.cc
+++ src/sqlite.cc
@@ -351,18 +351,18 @@ bf_sqlite_limit(Var arglist, Byte next, void *vdata, Objid progr)
         const char *str;
         int value;
     } categories[] = {
-        {"SQLITE_LIMIT_LENGTH", 0},
-        {"SQLITE_LIMIT_SQL_LENGTH", 1},
-        {"SQLITE_LIMIT_COLUMN", 2},
-        {"SQLITE_LIMIT_EXPR_DEPTH", 3},
-		{"SQLITE_LIMIT_COMPOUND_SELECT", 4},
-		{"SQLITE_LIMIT_VDBE_OP", 5},
-		{"SQLITE_LIMIT_FUNCTION_ARG", 6},
-		{"SQLITE_LIMIT_ATTACHED", 7},
-		{"SQLITE_LIMIT_LIKE_PATTERN_LENGTH", 8},
-		{"SQLITE_LIMIT_VARIABLE_NUMBER", 9},
-		{"SQLITE_LIMIT_TRIGGER_DEPTH", 10},
-		{"SQLITE_LIMIT_WORKER_THREADS", 11}
+        {"LIMIT_LENGTH", 0},
+        {"LIMIT_SQL_LENGTH", 1},
+        {"LIMIT_COLUMN", 2},
+        {"LIMIT_EXPR_DEPTH", 3},
+		{"LIMIT_COMPOUND_SELECT", 4},
+		{"LIMIT_VDBE_OP", 5},
+		{"LIMIT_FUNCTION_ARG", 6},
+		{"LIMIT_ATTACHED", 7},
+		{"LIMIT_LIKE_PATTERN_LENGTH", 8},
+		{"LIMIT_VARIABLE_NUMBER", 9},
+		{"LIMIT_TRIGGER_DEPTH", 10},
+		{"LIMIT_WORKER_THREADS", 11}
     };
 
     static const int max_categories = sizeof(categories) / sizeof(categories[0]);
