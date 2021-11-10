2805e791ad89f61f2ecdf83ba729603c4b4f9bb8
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2021-11-10 12:51:17 -0500

    Added connection caching for SQL and improved error handling further.

--- src/postgres.cc
+++ src/postgres.cc
@@ -15,16 +15,22 @@
 #include "server.h"
 #include "map.h"
 
-const char* connection_string()
+static pqxx::connection *cached_connection;
+static double last_connection_cleared;
+
+const char* 
+connection_string()
 {
-    char* s = "postgresql://";
+    char s[1024] = "";
+    strcat(s, "postgresql://");
     strcat(s, server_string_option("sql_user", "user"));
-    
+
     const char* sql_pass = server_string_option("sql_pass", "");
     if (sql_pass != "") {
         strcat(s, ":");
         strcat(s, sql_pass);
     }
+
     strcat(s, "@");
     strcat(s, server_string_option("sql_host", "localhost"));
 
@@ -36,11 +42,45 @@ const char* connection_string()
 
     strcat(s, "/");
     strcat(s, server_string_option("sql_database", "database"));
-
     return s;
 }
 
-Var 
+static pqxx::connection 
+*get_connection()
+{
+    try 
+    {
+        if (!cached_connection) {
+            oklog("creating new connection\n");
+            cached_connection = new pqxx::connection("postgresql://moo@localhost/moo"); //connection_string()
+            oklog("connection created\n");
+        }
+    }
+    catch (std::exception const &e)
+    {
+        oklog(e.what());
+
+        return nullptr;
+    }
+
+    return cached_connection;
+}
+
+static bool
+clear_connection()
+{
+    // This guard statement prevents connections from being
+    // created constantly in some sort of failure cascade.
+    const double curtime = time(nullptr);
+    if (last_connection_cleared == curtime)
+        return false;
+    
+    cached_connection = nullptr;
+    last_connection_cleared = curtime;
+    return true;
+}
+
+static Var 
 col2var(pqxx::field col) 
 {
     Var val;
@@ -71,7 +111,7 @@ col2var(pqxx::field col)
     return val;
 }
 
-Var
+static Var
 result2var(pqxx::result res)
 {
     Var ret = new_list(0);
@@ -85,7 +125,8 @@ result2var(pqxx::result res)
     return ret;
 }
 
-pqxx::params gen_parameters(Var *paramlist)
+static pqxx::params 
+gen_parameters(Var *paramlist)
 {
     pqxx::params p;
     for (int x = 1; x <= paramlist->v.num; x++) {
@@ -111,26 +152,46 @@ pqxx::params gen_parameters(Var *paramlist)
 void
 query_callback(const Var arglist, Var *ret)
 {
-    pqxx::connection c{connection_string()};
-    pqxx::work txn{c};
-
-    pqxx::result res;
 
     int nargs = arglist.v.list[0].v.num;
     try
     {
+        pqxx::connection *c = get_connection();
+        if (!c) {
+            *ret = str_dup_to_var("Connection refused to database.");
+            free_var(arglist);
+            return;
+        }
+
+        pqxx::work txn{*c};
+
+        pqxx::result res;
+        
         if (nargs < 2 || arglist.v.list[2].v.num < 1) {
             // There's no SQL parameters.
             res = txn.exec(arglist.v.list[1].v.str);
         } else {
             // Parameterize the SQL
-            c.prepare("sql_query", arglist.v.list[1].v.str);
+            c->prepare("sql_query", arglist.v.list[1].v.str);
             
             res = txn.exec_prepared("sql_query", gen_parameters(arglist.v.list[2].v.list));
         }
 
         *ret = result2var(res);
         txn.commit();
+    }
+    catch (pqxx::broken_connection const &e) 
+    {
+        // This can happen at literally any time.
+        // Clear the connection, begin again.
+        if (!clear_connection()) {
+            // If clear connection is false, we've exceeded retry attempts.
+            *ret = str_dup_to_var("Number of connection retries exceeded. SQL connection has failed.");
+            free_var(arglist);
+            return;
+        }
+        query_callback(arglist, ret);
+        return;
     }
     catch (std::exception const &e)
     {
@@ -143,6 +204,12 @@ query_callback(const Var arglist, Var *ret)
 static package 
 bf_sql_query(Var arglist, Byte next, void *vdata, Objid progr) 
 {
+    if (!is_wizard(progr))
+    {
+        free_var(arglist);
+        return make_error_pack(E_PERM);
+    }
+
     // Input validation for arguments.
     if (arglist.v.list[0].v.num == 2 && arglist.v.list[2].v.list->v.num > 0) {
         Var *tmp = arglist.v.list[2].v.list;
@@ -171,7 +238,11 @@ bf_sql_query(Var arglist, Byte next, void *vdata, Objid progr)
 static package 
 bf_sql_execute(Var arglist, Byte next, void *vdata, Objid progr) 
 {
-
+    if (!is_wizard(progr))
+    {
+        free_var(arglist);
+        return make_error_pack(E_PERM);
+    }
 }
 
 void register_postgres(void) 
